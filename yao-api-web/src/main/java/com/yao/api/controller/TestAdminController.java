feeb7bafe55579510b0006d43d03fec9388f4a83
yaohuanhuan <yaohuanhuan@great-tao.com>
2018-11-06 16:54:36 +0800

    修改配置

---
+++ yao-api-web/src/main/java/com/yao/api/controller/TestAdminController.java
@@ -0,0 +1,35 @@
+package com.yao.api.controller;
+
+
+
+import com.yao.api.model.User;
+import com.yao.api.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * Created by 姚焕焕 on 2018/08/09 09:06
+ * desc
+ * qq 1107129170@qq.com
+ */
+
+@Api(value = "value", description = "description")
+@Controller
+public class TestAdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @ApiOperation(value = "我是value", notes = "notes [@姚焕焕]")
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    @ResponseBody
+    public User test(){
+        return userService.register();
+    }
+}
